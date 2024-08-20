3d9f0cbc2bf04fde61b6129ce4ea4431b339bd09
skychaser <misha.kirillov.0990@gmail.com>
2024-08-20 18:55:20 +0300

    fix: remove lint-staged it not work

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,6 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{" "}
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
