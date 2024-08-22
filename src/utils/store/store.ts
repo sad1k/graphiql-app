ab377a1f22aa9475a066b339a50e2b2d386976ec
Dmitrii Samsonenko <samsondm97it@gmail.com>
2024-08-22 03:07:05 +0300

    build: add testing-library, add pre-push testing script

--- src/utils/store/store.ts
+++ src/utils/store/store.ts
@@ -1,10 +1,9 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit';
 
 export const store = configureStore({
-  reducer: {
-  }
-})
+  reducer: {},
+});
 
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
-export type AppStore = typeof store
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+export type AppStore = typeof store;
