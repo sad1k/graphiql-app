ad750e64b559ba50b6b0ee387f6b2195342f313b
Dmitrii Samsonenko <samsondm97it@gmail.com>
2024-08-25 16:30:40 +0300

    feat: add restclient page

---
+++ src/app/restclient/page.tsx
@@ -0,0 +1,9 @@
+const RestClient = (): JSX.Element => (
+  <main>
+    REST client
+    {/* TODO: add rest-edditor  view */}
+    {/* TODO: add response view */}
+  </main>
+);
+
+export default RestClient;
