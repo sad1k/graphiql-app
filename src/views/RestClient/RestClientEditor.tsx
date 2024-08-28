3876a6df2f3e057c25e48cecc82fe43bcb83df41
Dmitrii Samsonenko <samsondm97it@gmail.com>
2024-08-29 02:07:27 +0300

    feat: add page wrapper for Rest client editor

--- src/views/RestClient/RestClientEditor.tsx
+++ src/views/RestClient/RestClientEditor.tsx
@@ -1,5 +1,5 @@
 import { THeaders } from '@customTypes/headers';
-import { Grid } from '@mui/material';
+import { Grid, Paper } from '@mui/material';
 import { TMethod } from '@utils/restclient/method-type';
 import { v4 as uuidv4 } from 'uuid';
 import MethodSelect from './MethodSelect';
@@ -12,37 +12,39 @@ interface IRestClientEditor {
 
 const RestClientEditor = ({ method, url, headers }: IRestClientEditor) => (
   <section>
-    <Grid container spacing={3}>
-      <Grid item xs={12}>
-        <h3>RestClient Editor</h3>
-      </Grid>
+    <Paper sx={{ padding: 3 }}>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <h3>RestClient Editor</h3>
+        </Grid>
 
-      <Grid item xs={2}>
-        <MethodSelect initialValue={method} />
-      </Grid>
+        <Grid item xs={2}>
+          <MethodSelect initialValue={method} />
+        </Grid>
 
-      <Grid item xs={10}>
-        {/* TODO: add url input */}
-        <span>{url}</span>
-      </Grid>
+        <Grid item xs={10}>
+          {/* TODO: add url input */}
+          <span>{url}</span>
+        </Grid>
 
-      <Grid item xs={12}>
-        <h4>Headers: </h4>
-      </Grid>
+        <Grid item xs={12}>
+          <h4>Headers: </h4>
+        </Grid>
 
-      <Grid item xs={12}>
-        {/* TODO: add add-headers button */}
-        {headers.map(({ key, value }) => (
-          <li key={uuidv4()}>{`key: ${key}: value: ${value}`}</li>
-        ))}
-        {/* TODO: add headers table */}
-      </Grid>
+        <Grid item xs={12}>
+          {/* TODO: add add-headers button */}
+          {headers.map(({ key, value }) => (
+            <li key={uuidv4()}>{`key: ${key}: value: ${value}`}</li>
+          ))}
+          {/* TODO: add headers table */}
+        </Grid>
 
-      <Grid item xs={12}>
-        <h4>Body </h4>
-        {/* TODO: add JSON/Text Editor (the same component in response) */}
+        <Grid item xs={12}>
+          <h4>Body </h4>
+          {/* TODO: add JSON/Text Editor (the same component in response) */}
+        </Grid>
       </Grid>
-    </Grid>
+    </Paper>
   </section>
 );
 
