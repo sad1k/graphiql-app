e8d73e8a9b67d87ad69016ce3a76700aff29cfbd
Nikolaiev Mykhailo <ab3mn3@gmail.com>
2024-09-03 08:02:34 +0300

    refactor:remove StoreProvider from providers to app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 
 import ToastProvider from '@/providers/ToastProvider';
-import StoreProvider from '@/providers/StoreProvider';
+import StoreProvider from './StoreProvider';
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
