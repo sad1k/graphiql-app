04aada965d40bf6a5e91e38e9ef29f54e4cc61cc
Dmitrii Samsonenko <samsondm97it@gmail.com>
2024-09-04 17:13:15 +0300

    feat: add urlInput registration

--- src/views/RestClient/UrlInput.tsx
+++ src/views/RestClient/UrlInput.tsx
@@ -1,22 +1,34 @@
 'use client';
 
+import { IRestClientForm } from '@/types/rest-client-form';
 import { Grid, GridSize, TextField } from '@mui/material';
+import { Controller, useFormContext } from 'react-hook-form';
 
 interface IUrlInput {
   initialUrl: string;
   xs?: boolean | GridSize;
 }
 
-const UrlInput = ({ initialUrl, xs }: IUrlInput) => (
-  <Grid item xs={xs}>
-    <TextField
-      id='url-input'
-      variant='outlined'
-      fullWidth
-      defaultValue={initialUrl}
-      placeholder='Enter URL'
-    />
-  </Grid>
-);
+const UrlInput = ({ initialUrl, xs }: IUrlInput) => {
+  const { control } = useFormContext<IRestClientForm>();
+  return (
+    <Grid item xs={xs}>
+      <Controller
+        name='url'
+        control={control}
+        defaultValue={initialUrl}
+        render={({ field }) => (
+          <TextField
+            {...field}
+            id='url-input'
+            variant='outlined'
+            fullWidth
+            placeholder='Enter URL'
+          />
+        )}
+      />
+    </Grid>
+  );
+};
 
 export default UrlInput;
