fc529c350502c1c0fbb5ed8f98819f6fffbee1e1
ab3MN <ab3mn3@gmail.com>
2024-09-12 15:05:19 +0300

    feat:add anchor to useScrolling

--- src/hooks/useScrolling.ts
+++ src/hooks/useScrolling.ts
@@ -4,13 +4,20 @@ import _debounce from 'lodash/debounce';
 
 const handleDebounceTime = 100;
 
-const useScrolling = () => {
-  let prevPosition = window.scrollY;
+const maxScrolDiff = 25;
+const minScrolDiff = -25;
 
-  const [scroll, setScroll] = useState(Boolean(window.scrollY));
+const useScrolling = () => {
+  let prevPosition = window ? window.scrollY : 0;
+
+  const [scroll, setScroll] = useState(Boolean(prevPosition));
 
   const handleScroll = _debounce(() => {
     const currentPosition = window.scrollY;
+
+    const diff = prevPosition - currentPosition;
+
+    if (diff < maxScrolDiff && diff > -minScrolDiff) return;
 
     prevPosition < currentPosition ? setScroll(true) : setScroll(false);
     prevPosition = currentPosition;
