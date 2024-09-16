9bd9fddd2071ed40fc927e687685ba2a56e39c21
skychaser <misha.kirillov.0990@gmail.com>
2024-09-16 16:17:13 +0300

    fix: fix poblems with eslint

--- src/tests/graphql/updateBodyInUrl.test.ts
+++ src/tests/graphql/updateBodyInUrl.test.ts
@@ -16,23 +16,24 @@ test('updateBodyInUrl error with url', () => {
 test('updateBodyInUrl with url', () => {
   const newBody = 'newBody';
 
-  window = Object.create(window) as typeof globalThis & Window;
+  global.window = Object.create(window) as typeof globalThis & Window;
 
+  const myWindow = global.window;
   const url =
     'http://localhost:3000/graphql/aHR0cHM6Ly9ncmFwaHFsLXBva2Vtb24yLnZlcmNlbC5hcHA=/cXVlcnl7Cglwb2tlbW9ucyhmaXJzdDogMSl7CiAgCW5hbWU6IG5hbWUKICAgIGNsYXNzaWZpY2F0aW9uOiBjbGFzc2lmaWNhdGlvbgogIH0KfQ==';
 
-  Object.defineProperty(window, 'location', {
+  Object.defineProperty(myWindow, 'location', {
     value: {
       href: url,
     },
     writable: true,
   });
-  Object.defineProperty(window, 'history', {
+  Object.defineProperty(myWindow, 'history', {
     value: {
       pushState: vi.fn(),
     },
     writable: true,
   });
   updateBodyInUrl(newBody);
-  expect(window.history.pushState).toBeCalledTimes(1);
+  expect(myWindow.history.pushState).toBeCalledTimes(1);
 });
