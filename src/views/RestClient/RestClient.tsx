7c47aeb100327ee21dc422552d0ea22ceba68956
Dmitrii Samsonenko <samsondm97it@gmail.com>
2024-09-16 23:08:37 +0300

    feat: add redirecting on home from rest client for unauthorized

--- src/views/RestClient/RestClient.tsx
+++ src/views/RestClient/RestClient.tsx
@@ -1,16 +1,26 @@
 'use client';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Stack } from '@mui/material';
 import { IRestClientInputs } from '@/types/rest-client-form';
-import RestClientEditor from './RestClientEditor';
+import { useAppSelector } from '@/utils/store/hooks';
+import { HOME } from '@/constants/path';
+import { redirect } from 'next/navigation';
 import RestClientResponse from './RestClientResponse';
+import RestClientEditor from './RestClientEditor';
 
 const INITIAL_STATUS = 0;
 
 const RestClient = ({ method, url, headers, body }: IRestClientInputs) => {
   const [response, setResponse] = useState<string>('');
   const [status, setStatus] = useState<number>(INITIAL_STATUS);
+  const authState = useAppSelector((state) => state.auth.authState);
+
+  useEffect(() => {
+    if (!authState) {
+      redirect(HOME);
+    }
+  }, []);
 
   return (
     <Stack
